6be45456e559b7bcc8e843d3ef98664daffc0540
subham8757 <subhammazumdar8757@gmail.com>
2024-03-08 17:46:05 +0530

    update

--- Authentication/Login page/login.js
+++ Authentication/Login page/login.js
@@ -1,49 +1,48 @@
+document.getElementById("createbtn").addEventListener("click", function () {
+  let a = document.createElement("a");
+  a.setAttribute("href", "../sign up/signup.html");
+  a.click();
 
+  console.log("hello");
+});
 
-document.getElementById("createbtn").addEventListener("click",function(){
-    let a =document.createElement("a")
-    a.setAttribute("href","../sign up/signup.html")
-    a.click()
+let signupdata = JSON.parse(localStorage.getItem("signup"));
 
-    console.log("hello")
-})
-
-
-let signupdata=JSON.parse(localStorage.getItem("signup"))
-
-if(signupdata===null){
-    signupdata=[]
+if (signupdata === null) {
+  signupdata = [];
 }
 
-let users=[
-    {name:"suryakant",mobile:"1234567890",email:"abc@gmail.com",password:"123"}
-]
+let users = [
+  {
+    name: "suryakant",
+    mobile: "1234567890",
+    email: "abc@gmail.com",
+    password: "123",
+  },
+];
 
+let allusers = [];
 
-let allusers=[]
+allusers = [...signupdata, ...users];
 
-allusers=[...signupdata,...users]
+console.log(allusers);
 
-console.log(allusers)
+let myform = document.getElementById("form");
 
+myform.addEventListener("submit", function (event) {
+  event.preventDefault();
 
-let myform=document.getElementById("form")
+  let emailorphone = document.getElementById("emailorphone").value;
+  let password = document.getElementById("password").value;
 
-myform.addEventListener("submit",function(event){
-    event.preventDefault()
-
-    let emailorphone=document.getElementById("emailorphone").value
-    let password=document.getElementById("password").value
-
-    for(let i=0; i<allusers.length; i++){
-        if(allusers[i].email===emailorphone || allusers[i].mobile===emailorphone && allusers[i].password===password){
-            let a =document.createElement("a")
-            a.setAttribute("href","../../Home page/index.html")
-            a.click()
-        }
+  for (let i = 0; i < allusers.length; i++) {
+    if (
+      allusers[i].email === emailorphone ||
+      (allusers[i].mobile === emailorphone && allusers[i].password === password)
+    ) {
+      let a = document.createElement("a");
+      a.setAttribute("href", "../../Home page/index.html");
+      a.click();
     }
-})
-
-
-
-
+  }
+});
--- Authentication/sign up/signup.js
+++ Authentication/sign up/signup.js
@@ -1,57 +1,46 @@
-
-
-
-let data=JSON.parse(localStorage.getItem("signup"))
-if(data==null){
-    data=[]
+let data = JSON.parse(localStorage.getItem("signup"));
+if (data == null) {
+  data = [];
 }
 
-let myform=document.getElementById("form")
+let myform = document.getElementById("form");
 
+myform.addEventListener("submit", function (event) {
+  event.preventDefault();
 
-myform.addEventListener("submit",function(event){
-    event.preventDefault()
+  let name = document.getElementById("name").value;
+  let mobile = document.getElementById("mobile").value;
+  let email = document.getElementById("email").value;
+  let password = document.getElementById("password").value;
 
-    
+  let obj = {
+    name,
+    mobile,
+    email,
+    password,
+  };
 
-    let name =document.getElementById("name").value
-    let mobile=document.getElementById("mobile").value
-    let email=document.getElementById("email").value
-    let password=document.getElementById("password").value
-
-
-    let obj={
-        name,
-        mobile,
-        email,
-        password,
+  let flag = false;
+  for (let i = 0; i < data.length; i++) {
+    if (data[i].mobile === mobile || data[i].email === email) {
+      flag = true;
+      break;
     }
-    
-    let flag=false
-    for(let i=0; i<data.length; i++){
-        if(data[i].mobile===mobile || data[i].email===email){
-            flag=true;
-            break;
-        }
-    }
+  }
 
+  if (flag) {
+    alert("User already exist");
+  } else {
+    data.push(obj);
 
-    if(flag){
-        alert("User already exist")
-    }else{
-        data.push(obj)
-    
-        localStorage.setItem("signup",JSON.stringify(data))
-        let a=document.createElement("a")
-        a.setAttribute("href","../Login page/login.html")
-        a.click()
-    }
-    
-    document.getElementById("name").value=""
-    document.getElementById("mobile").value=""
-    document.getElementById("email").value=""
-    document.getElementById("password").value=""
+    localStorage.setItem("signup", JSON.stringify(data));
+    let a = document.createElement("a");
+    a.setAttribute("href", "../Login page/login.html");
+    a.click();
+  }
 
-
-    
-})
+  document.getElementById("name").value = "";
+  document.getElementById("mobile").value = "";
+  document.getElementById("email").value = "";
+  document.getElementById("password").value = "";
+});
